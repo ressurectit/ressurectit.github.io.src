da210f6198f14d3b8584686d5ae910951f40d1c4
kukjevov <kukjevov@gmail.com>
2026-05-21 10:42:46 +0200

    INT: updated customized view sample

--- app/samples/grid/customizedView/customizedViewSample.component.ts
+++ app/samples/grid/customizedView/customizedViewSample.component.ts
@@ -1,4 +1,9 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {AsyncDataLoaderOptions, DataResponse, GridOptions, MatrixGridModule, SimpleOrdering} from '@anglr/grid';
+import {RecursivePartial} from '@jscrpt/common';
+import {lastValueFrom} from '@jscrpt/common/rxjs';
+
+import {Address, DataService} from '../../../services/api/data';
 
 /**
  * Customized View sample for grid component
@@ -9,9 +14,81 @@ import {Component, ChangeDetectionStrategy} from '@angular/core';
     templateUrl: 'customizedViewSample.component.html',
     imports:
     [
+        MatrixGridModule,
+    ],
+    providers:
+    [
+        DataService,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomizedViewSampleComponent
 {
+    //######################### protected properties - template bindings #########################
+
+    /**
+     * Grid options that are used for grid initialization
+     */
+    protected gridOptions: RecursivePartial<GridOptions>;
+
+    /**
+     * Grid options that are used for grid initialization for gallery sample
+     */
+    protected galleryGridOptions: RecursivePartial<GridOptions>;
+
+    //######################### constructor #########################
+    constructor(private _dataSvc: DataService)
+    {
+        this.gridOptions =
+        {
+            plugins:
+            {
+                dataLoader:
+                {
+                    options: <AsyncDataLoaderOptions<Address, SimpleOrdering>>
+                    {
+                        //data callback used for getting data asynchronously
+                        dataCallback: this._getData.bind(this),
+                    },
+                },
+            },
+        };
+
+        this.galleryGridOptions =
+        {
+            plugins:
+            {
+                dataLoader:
+                {
+                    options: <AsyncDataLoaderOptions<Address, SimpleOrdering>>
+                    {
+                        //data callback used for getting data asynchronously
+                        dataCallback: this._getData.bind(this),
+                    },
+                },
+            },
+        };
+    }
+
+    //######################### private methods #########################
+
+    /**
+     * Callback used for obtaining data
+     * @param page - Index of requested page
+     * @param itemsPerPage - Number of items per page
+     * @param ordering - Order by column name
+     */
+    private async _getData(page: number, itemsPerPage: number, ordering: SimpleOrdering): Promise<DataResponse<Address>>
+    {
+        const result = await lastValueFrom(this._dataSvc.getData({
+                                                                     page: page,
+                                                                     size: itemsPerPage,
+                                                                 },
+                                                                 ordering));
+
+        return {
+            data: result?.content ?? [],
+            totalCount: result?.totalElements ?? 0,
+        };
+    }
 }
