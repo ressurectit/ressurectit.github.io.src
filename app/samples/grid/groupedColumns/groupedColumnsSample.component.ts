60b5440f340034a0dd7d1ea5da188931ddce2094
kukjevov <kukjevov@gmail.com>
2026-05-21 13:23:20 +0200

    INT: updated html table and grouped columns samples

--- app/samples/grid/groupedColumns/groupedColumnsSample.component.ts
+++ app/samples/grid/groupedColumns/groupedColumnsSample.component.ts
@@ -1,4 +1,9 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {AsyncDataLoaderOptions, BasicPagingOptions, DataResponse, GridOptions, MatrixGridModule, SimpleOrdering} from '@anglr/grid';
+import {RecursivePartial} from '@jscrpt/common';
+import {lastValueFrom} from '@jscrpt/common/rxjs';
+
+import {Address, DataService} from '../../../services/api/data';
 
 /**
  * Grouped Columns sample for grid component
@@ -9,9 +14,71 @@ import {Component, ChangeDetectionStrategy} from '@angular/core';
     templateUrl: 'groupedColumnsSample.component.html',
     imports:
     [
+        MatrixGridModule,
+    ],
+    providers:
+    [
+        DataService,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GroupedColumnsSampleComponent
 {
+    //######################### protected properties - template bindings #########################
+
+    /**
+     * Grid options that are used for grid initialization
+     */
+    protected gridOptions: RecursivePartial<GridOptions>;
+
+    //######################### constructor #########################
+    constructor(private _dataSvc: DataService)
+    {
+        this.gridOptions =
+        {
+            plugins:
+            {
+                dataLoader:
+                {
+                    options: <AsyncDataLoaderOptions<Address, SimpleOrdering>>
+                    {
+                        //data callback used for getting data asynchronously
+                        dataCallback: this._getData.bind(this),
+                    },
+                },
+                paging:
+                {
+                    options: <BasicPagingOptions>
+                    {
+                        //available values for items per page buttons
+                        itemsPerPageValues: [5, 10, 20],
+                        //initial value of items per page, should be one of above
+                        initialItemsPerPage: 5,
+                    },
+                },
+            },
+        };
+    }
+
+    //######################### private methods #########################
+
+    /**
+     * Callback used for obtaining data
+     * @param page - Index of requested page
+     * @param itemsPerPage - Number of items per page
+     * @param ordering - Order by column name
+     */
+    private async _getData(page: number, itemsPerPage: number, ordering: SimpleOrdering): Promise<DataResponse<Address>>
+    {
+        const result = await lastValueFrom(this._dataSvc.getData({
+                                                                        page: page,
+                                                                        size: itemsPerPage,
+                                                                    },
+                                                                    ordering));
+
+        return {
+            data: result?.content ?? [],
+            totalCount: result?.totalElements ?? 0,
+        };
+    }
 }
--- app/samples/grid/htmlTable/htmlTableSample.component.ts
+++ app/samples/grid/htmlTable/htmlTableSample.component.ts
@@ -1,4 +1,9 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {AsyncDataLoaderOptions, DataResponse, GridOptions, MatrixGridModule, SimpleOrdering, UseTableDirective} from '@anglr/grid';
+import {RecursivePartial} from '@jscrpt/common';
+import {lastValueFrom} from '@jscrpt/common/rxjs';
+
+import {Address, DataService} from '../../../services/api/data';
 
 /**
  * Html Table sample for grid component
@@ -9,9 +14,62 @@ import {Component, ChangeDetectionStrategy} from '@angular/core';
     templateUrl: 'htmlTableSample.component.html',
     imports:
     [
+        MatrixGridModule,
+        UseTableDirective,
+    ],
+    providers:
+    [
+        DataService,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HtmlTableSampleComponent
 {
+    //######################### protected properties - template bindings #########################
+
+    /**
+     * Grid options that are used for grid initialization
+     */
+    protected gridOptions: RecursivePartial<GridOptions>;
+
+    //######################### constructor #########################
+    constructor(private _dataSvc: DataService)
+    {
+        this.gridOptions =
+        {
+            plugins:
+            {
+                dataLoader:
+                {
+                    options: <AsyncDataLoaderOptions<Address, SimpleOrdering>>
+                    {
+                        //data callback used for getting data asynchronously
+                        dataCallback: this._getData.bind(this),
+                    },
+                },
+            },
+        };
+    }
+
+    //######################### private methods #########################
+
+    /**
+     * Callback used for obtaining data
+     * @param page - Index of requested page
+     * @param itemsPerPage - Number of items per page
+     * @param ordering - Order by column name
+     */
+    private async _getData(page: number, itemsPerPage: number, ordering: SimpleOrdering): Promise<DataResponse<Address>>
+    {
+        const result = await lastValueFrom(this._dataSvc.getData({
+                                                                        page: page,
+                                                                        size: itemsPerPage,
+                                                                    },
+                                                                    ordering));
+
+        return {
+            data: result?.content ?? [],
+            totalCount: result?.totalElements ?? 0,
+        };
+    }
 }
